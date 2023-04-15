c2dcddedf1a1cd2db776caac0ce6703d16e4f0c3
Aang Muammar Zein <amuammarzein@gmail.com>
2023-04-15 22:00:47 +0700

    add sound fx for tutorial mode

---
+++ rahmath/VideoView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoView.swift
+//  Rahmath
+//
+//  Created by Aang Muammar Zein on 15/04/23.
+//
+
+import SwiftUI
+
+struct VideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView()
+    }
+}
